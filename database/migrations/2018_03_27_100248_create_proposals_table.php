88f2fab15d2f09cf84f23c0b98264edcdb1a1547
Marcus Vinícius Campos <marcus.campos@sympla.com.br>
2018-03-27 10:25:44 -0300

    Proposal and coments table

---
+++ database/migrations/2018_03_27_100248_create_proposals_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->double('net_value', 10, 2);
+            $table->double('gross_value', 10, 2);
+            $table->double('time_to_finish_the_job', 4, 2)->nullable();
+            $table->boolean('promoted')->default(false);
+            $table->boolean('accepts')->default(false);
+            $table->integer('user_id')->unsigned();
+            $table->integer('job_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('job_id')->references('id')->on('jobs');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposals');
+    }
+}
---
+++ database/migrations/2018_03_27_100352_create_proposal_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProposalCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposal_comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->integer('user_id')->unsigned();
+            $table->integer('proposal_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('proposal_id')->references('id')->on('proposals');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal_comments');
+    }
+}
