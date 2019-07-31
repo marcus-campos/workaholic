147a9410e9e30555bad8f6507586467d01563958
Marcus Vinícius Campos <marcus_vinicius_campos@hotmail.com>
2019-07-31 20:48:27 -0300

    email

--- resources/views/vendor/template/layout.blade.php
+++ resources/views/vendor/template/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Plataforma para unir clientes e prestadores de serviços.">
-        <meta name="author" content="Marcus Vinícius Campos <campos.v.marcus@gmail.com>">
+        <meta name="author" content="Marcus Vinícius Campos">
 
         <!-- <link rel="shortcut icon" href=""> -->
 
